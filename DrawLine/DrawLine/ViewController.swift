f416d392eb901e14328ced37dce2c90d61b6d502
Jonas Reinsch <jonas.reinsch@gmail.com>
2015-09-14 15:22:57 +0200

    add drag line demo

---
+++ DrawLine/DrawLine/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  DrawLine
+//
+//  Created by Jonas Reinsch on 14.09.15.
+//  Copyright (c) 2015 Jonas Reinsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor.darkGrayColor()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
