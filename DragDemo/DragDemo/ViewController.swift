1c713a07049b9278dd8150a83ddcdbd052b85bf1
Jonas Reinsch <jonas.reinsch@gmail.com>
2015-09-14 11:18:00 +0200

    add drag demo

---
+++ DragDemo/DragDemo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  DragDemo
+//
+//  Created by Jonas Reinsch on 12.09.15.
+//  Copyright (c) 2015 Jonas Reinsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.greenColor()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
