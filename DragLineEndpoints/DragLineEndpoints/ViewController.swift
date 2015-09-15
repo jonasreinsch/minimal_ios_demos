1c2b03c963efb5227d502ce64f34310118bf7a28
Jonas Reinsch <jonas.reinsch@gmail.com>
2015-09-15 11:39:27 +0200

    update .gitignore, add new project

---
+++ DragLineEndpoints/DragLineEndpoints/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DragLineEndpoints
+//
+//  Created by Jonas Reinsch on 15.09.15.
+//  Copyright (c) 2015 Jonas Reinsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
