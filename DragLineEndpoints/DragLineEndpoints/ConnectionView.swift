cb5d56374e692a5ad93745036694707a7b92067a
Jonas Reinsch <jonas.reinsch@gmail.com>
2015-09-16 06:23:29 +0200

    add new file ConnectionView

---
+++ DragLineEndpoints/DragLineEndpoints/ConnectionView.swift
@@ -0,0 +1,21 @@
+//
+//  ConnectionView.swift
+//  DragLineEndpoints
+//
+//  Created by Jonas Reinsch on 16.09.15.
+//  Copyright (c) 2015 Jonas Reinsch. All rights reserved.
+//
+
+import UIKit
+
+class ConnectionView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
