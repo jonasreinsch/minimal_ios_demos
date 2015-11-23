6cc2c1c0b5bfcc70dc746e500701d659861e6be1
Jonas Reinsch <jonas.reinsch@gmail.com>
2015-11-23 10:50:05 +0100

    slider values get extracted and displayed

--- BlurDemo/BlurDemo/ViewController.swift
+++ BlurDemo/BlurDemo/ViewController.swift
@@ -13,6 +13,17 @@ let imageFilename = "moscow_city.jpg"
 
 class ViewController: UIViewController {
     let stackView = UIStackView()
+    let labels = [UILabel(), UILabel(), UILabel()]
+    let sliders = [UISlider(), UISlider(), UISlider()]
+    
+    let effectStyles = [(constant:UIBlurEffectStyle.ExtraLight, name:"Extra Light"),
+        (constant:UIBlurEffectStyle.Light, name:"Light"),
+        (constant:UIBlurEffectStyle.Dark, name:"Dark")]
+    
+    func makeLabelText(idx:Int, value:Float) -> String {
+        let valueInPercent = Int(round(value * 100))
+        return("Blur Effect Style: \(effectStyles[idx].name) (\(valueInPercent)% blurred)")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,50 +37,52 @@ class ViewController: UIViewController {
         stackView.leftAnchor.constraintEqualToAnchor(view.leftAnchor).active = true
         stackView.rightAnchor.constraintEqualToAnchor(view.rightAnchor).active = true
         
-        stackView.alignment = .Center
+        stackView.alignment = .Fill
         stackView.distribution = .FillEqually
+        
+
+        
         for var i=0; i != 3; ++i {
             let innerStackView = UIStackView()
             innerStackView.distribution = .Fill
             innerStackView.alignment = .Center
             innerStackView.axis = .Vertical
-            
-            let label = UILabel()
-            label.translatesAutoresizingMaskIntoConstraints = false
-
-            label.text = "Test"
-            label.backgroundColor = UIColor.greenColor()
         
             let imageView = UIImageView(image: UIImage(named: imageFilename))
+            imageView.backgroundColor = UIColor.blackColor()
             imageView.contentMode = .ScaleAspectFit
             imageView.translatesAutoresizingMaskIntoConstraints = false
             
-            let slider = UISlider()
-            slider.translatesAutoresizingMaskIntoConstraints = false
+            let effect = UIBlurEffect(style: .Dark)
+            print(UIBlurEffectStyle.Dark.rawValue)
+            
+            labels[i].translatesAutoresizingMaskIntoConstraints = false
+
+            sliders[i].translatesAutoresizingMaskIntoConstraints = false
+            sliders[i].tag = i
+            
+            labels[i].text = makeLabelText(i, value: 0)
 
             imageView.setContentHuggingPriority(249, forAxis: .Vertical)
             imageView.setContentCompressionResistancePriority(749, forAxis: .Vertical)
             innerStackView.addArrangedSubview(imageView)
-            innerStackView.addArrangedSubview(label)
-            innerStackView.addArrangedSubview(slider)
-            slider.leadingAnchor.constraintEqualToAnchor(innerStackView.leadingAnchor, constant: 20).active = true
-            slider.trailingAnchor.constraintEqualToAnchor(innerStackView.trailingAnchor, constant: -20).active = true
+            innerStackView.addArrangedSubview(labels[i])
+            innerStackView.addArrangedSubview(sliders[i])
+            sliders[i].leadingAnchor.constraintEqualToAnchor(innerStackView.leadingAnchor, constant: 20).active = true
+            sliders[i].trailingAnchor.constraintEqualToAnchor(innerStackView.trailingAnchor, constant: -20).active = true
+            sliders[i].addTarget(self, action: "sliderDragged:", forControlEvents: .ValueChanged)
             
             stackView.addArrangedSubview(innerStackView)
         }
-        
-
-        
-        
-
-        
-        
-
-        
     }
     
     override func viewDidLayoutSubviews() {
 
+    }
+    
+    func sliderDragged(slider:UISlider) {
+        print(slider.value, slider.tag)
+        labels[slider.tag].text = makeLabelText(slider.tag, value:slider.value)
     }
 
     override func didReceiveMemoryWarning() {
