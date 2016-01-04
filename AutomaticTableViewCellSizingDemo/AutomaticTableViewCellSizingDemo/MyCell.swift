5fa2eefe151623742d0832316032e69ab4cfd3d5
Jonas Reinsch <jonas.reinsch@gmail.com>
2016-01-04 16:46:51 +0100

    refactor constraints to nslayoutanchors

--- AutomaticTableViewCellSizingDemo/AutomaticTableViewCellSizingDemo/MyCell.swift
+++ AutomaticTableViewCellSizingDemo/AutomaticTableViewCellSizingDemo/MyCell.swift
@@ -39,17 +39,14 @@ class MyCell: UITableViewCell {
         contentView.addSubview(myLabel)
         myLabel.translatesAutoresizingMaskIntoConstraints = false
         myLabel.numberOfLines = 0
-
-        let leftConstraint = NSLayoutConstraint(item: myLabel, attribute: .Left, relatedBy: .Equal, toItem: contentView, attribute: .Left, multiplier: 1, constant: 20)
-        let topConstraint = NSLayoutConstraint(item: myLabel, attribute: .Top, relatedBy: .Equal, toItem: contentView, attribute: .Top, multiplier: 1, constant: 20)
-        let bottomConstraint = NSLayoutConstraint(item: myLabel, attribute: .Bottom, relatedBy: .Equal, toItem: contentView, attribute: .Bottom, multiplier: 1, constant: -20)
-        let rightConstraint = NSLayoutConstraint(item: myLabel, attribute: .Right, relatedBy: .Equal, toItem: contentView, attribute: .Right, multiplier: 1, constant: -20)
         
-        contentView.addConstraints([leftConstraint, topConstraint, bottomConstraint, rightConstraint])
+        myLabel.leadingAnchor.constraintEqualToAnchor(contentView.leadingAnchor, constant: 20).active = true
+        myLabel.trailingAnchor.constraintEqualToAnchor(contentView.trailingAnchor, constant: -20).active = true
+        myLabel.topAnchor.constraintEqualToAnchor(contentView.topAnchor, constant: 20).active = true
+        myLabel.bottomAnchor.constraintEqualToAnchor(contentView.bottomAnchor, constant: -20).active = true
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
 }
