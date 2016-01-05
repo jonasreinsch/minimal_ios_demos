65241f1cc5e7d53d87236941abe7459f0c22df69
Jonas Reinsch <jonas.reinsch@gmail.com>
2016-01-05 15:15:24 +0100

    eliminate image cache

--- PhotosDemo/PhotosDemo/PhotosCollectionViewController.swift
+++ PhotosDemo/PhotosDemo/PhotosCollectionViewController.swift
@@ -1,19 +1,3 @@
-//
-// Copyright 2014 Scott Logic
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-
 import UIKit
 import Photos
 
@@ -24,10 +8,8 @@ class PhotosCollectionViewController: UICollectionViewController,
 {
     var images: PHFetchResult!
 
-    let imageManager = PHCachingImageManager()
+    let imageManager = PHImageManager()
 
-    var imageCacheController: ImageCacheController!
-  
     override func viewDidLoad() {
         super.viewDidLoad()
     
@@ -39,11 +21,9 @@ class PhotosCollectionViewController: UICollectionViewController,
                                      forCellWithReuseIdentifier: reuseIdentifier)
     
         images = PHAsset.fetchAssetsWithMediaType(.Image, options: nil)
-        imageCacheController = ImageCacheController(imageManager: imageManager,
-                                                    images: images,
-                                                     preheatSize: 1)
-        
         PHPhotoLibrary.sharedPhotoLibrary().registerChangeObserver(self)
+        
+        print(images.count)
     }
     
     override func viewWillLayoutSubviews() {
@@ -64,17 +44,10 @@ class PhotosCollectionViewController: UICollectionViewController,
   override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
     let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! PhotosCollectionViewCell
     
-    // Configure the cell
     cell.imageManager = imageManager
     cell.imageAsset = images[images.count - (indexPath.item + 1)] as? PHAsset
     
     return cell
-  }
-  
-  // MARK: - ScrollViewDelegate
-  override func scrollViewDidScroll(scrollView: UIScrollView) {
-    let indexPaths = collectionView?.indexPathsForVisibleItems()
-    imageCacheController.updateVisibleCells(indexPaths as [NSIndexPath]!)
   }
   
   // MARK: - PHPhotoLibraryChangeObserver
