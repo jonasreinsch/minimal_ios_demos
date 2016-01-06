8d1805d996546ba16a5d5592e54635b304949f59
Jonas Reinsch <jonas.reinsch@gmail.com>
2016-01-06 22:00:54 +0100

    add live camera stream demo

---
+++ LiveCameraStreamDemo/LiveCameraStreamDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LiveCameraStreamDemo
+//
+//  Created by Jonas Reinsch on 06.01.16.
+//  Copyright © 2016 Jonas Reinsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
