a4fb64d92cd8066782261bc29525d07ab234716d
Jonas Reinsch <jonas.reinsch@gmail.com>
2016-01-07 10:08:56 +0100

    basic live camera stream demo works

--- LiveCameraStreamDemo/LiveCameraStreamDemo/ViewController.swift
+++ LiveCameraStreamDemo/LiveCameraStreamDemo/ViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    let captureSession = AVCaptureSession()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Use the default camera (usually the back camera)
+        // If you want the front camera instead
+        // loop through AVCaptureDevice.devices() and use the
+        // following check:
+        // device.hasMediaType(AVMediaTypeVideo) &&
+        // (device.position == AVCaptureDevicePosition.Front)
+        
+        // get the default capture device for video (back camera usually)
+        guard let captureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo) else {
+            fatalError("capture device was nil")
+        }
+        
+        // get input from the camera, configure the capture session
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            captureSession.addInput(input)
+        } catch {
+            fatalError("can't access camera")
+        }
+        captureSession.sessionPreset = AVCaptureSessionPresetHigh
+        captureSession.startRunning()
+        
+        // configure the preview layer
+        guard let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession) else {
+            fatalError("preview layer was nil")
+        }
+        previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        previewLayer.frame = view.layer.bounds
+        view.layer.addSublayer(previewLayer)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
