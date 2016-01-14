6b8d56fe4b568eb7fc4495c985a70e71a2e577e6
Jonas Reinsch <jonas.reinsch@gmail.com>
2016-01-14 11:34:13 +0100

    add image picker overlay demo

---
+++ UIImagePickerOverlayDemo/UIImagePickerOverlayDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIImagePickerOverlayDemo
+//
+//  Created by Jonas Reinsch on 14.01.16.
+//  Copyright © 2016 Jonas Reinsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
