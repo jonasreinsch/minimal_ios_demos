4479825cbbf93619201564a5d2b84393d907ae7d
Jonas Reinsch <jonas.reinsch@gmail.com>
2016-01-14 17:43:20 +0100

    start supporting the iPad in both orientations

--- UIImagePickerOverlayDemo/UIImagePickerOverlayDemo/ViewController.swift
+++ UIImagePickerOverlayDemo/UIImagePickerOverlayDemo/ViewController.swift
@@ -43,7 +43,6 @@ class ViewController: UIViewController {
         // is the default. But better to make this explicit.
         imagePicker.mediaTypes = [kUTTypeImage as String]
 
-//             imagePicker.delegate = self;
         
         let tgr = UITapGestureRecognizer(target: self, action: "didTap")
         view.addGestureRecognizer(tgr)
@@ -55,9 +54,23 @@ class ViewController: UIViewController {
             overlay.hidden = false
         }
         
+        NSNotificationCenter.defaultCenter().addObserverForName("UIDeviceOrientationDidChangeNotification", object:nil, queue:nil) { note in
+            print("orientation did change: \(UIDevice.currentDevice().orientation.rawValue)")
+        }
+        
     }
     
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+        
+        print(UIDevice.currentDevice().orientation)
+    }
+    
+    
     func didTap() {
+        frameView.heightAnchor.constraintEqualToAnchor(overlay.heightAnchor, multiplier: 0.8).active = true
+        frameView.widthAnchor.constraintEqualToAnchor(frameView.heightAnchor, multiplier: 1 / 1.545454).active = true
+        
         self.presentViewController(self.imagePicker, animated: true) {
             // important when showing the photo picker a second time,
             // since on _UIImagePickerControllerUserDidCaptureItem it gets hidden
@@ -67,10 +80,18 @@ class ViewController: UIViewController {
             case .Pad:
                 topOffset = 0
                 overlay.bottomAnchor.constraintEqualToAnchor(overlay.superview!.bottomAnchor).active = true
+                if UIDeviceOrientationIsLandscape(UIDevice.currentDevice().orientation) {
+                    frameView.widthAnchor.constraintEqualToAnchor(overlay.widthAnchor, multiplier: 0.8).active = true
+                    frameView.heightAnchor.constraintEqualToAnchor(frameView.widthAnchor, multiplier: 1 / 1.545454).active = true
+
+                }
+
             case .Phone:
                 topOffset = 44
                 // height / width = 4 / 3 (aspect ratio)
                 overlay.heightAnchor.constraintEqualToAnchor(overlay.widthAnchor, multiplier: 4/3).active = true
+                
+
             case .TV:
                 fatalError("not supposed to run on TV")
             case .Unspecified:
@@ -81,8 +102,8 @@ class ViewController: UIViewController {
             overlay.trailingAnchor.constraintEqualToAnchor(overlay.superview!.trailingAnchor).active = true
             overlay.topAnchor.constraintEqualToAnchor(overlay.superview!.topAnchor, constant: topOffset).active = true
 
-            frameView.heightAnchor.constraintEqualToAnchor(overlay.heightAnchor, multiplier: 0.8).active = true
-            frameView.widthAnchor.constraintEqualToAnchor(frameView.heightAnchor, multiplier: 1 / 1.545454).active = true
+            
+
             
             frameView.centerXAnchor.constraintEqualToAnchor(overlay.centerXAnchor).active = true
             frameView.centerYAnchor.constraintEqualToAnchor(overlay.centerYAnchor).active = true
