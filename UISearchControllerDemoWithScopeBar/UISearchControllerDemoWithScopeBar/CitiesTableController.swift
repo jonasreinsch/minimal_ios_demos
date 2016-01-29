53dc8357b14171d9663c7769a6d14b309247195c
Jonas Reinsch <jonas.reinsch@gmail.com>
2016-01-29 11:04:04 +0100

    basic filtering works

--- UISearchControllerDemoWithScopeBar/UISearchControllerDemoWithScopeBar/CitiesTableController.swift
+++ UISearchControllerDemoWithScopeBar/UISearchControllerDemoWithScopeBar/CitiesTableController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
-class CitiesTableController: UITableViewController {
+func searchControllerActiveAndNotEmpty(searchController:UISearchController) -> Bool {
+    guard let text = searchController.searchBar.text else {
+        return false
+    }
 
+    return searchController.active && !text.isEmpty
+}
+
+class CitiesTableController: UITableViewController {
+    let searchController = UISearchController(searchResultsController: nil)
+    var filteredCities:[City] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "Search Cities"
+        
+        tableView.tableHeaderView = searchController.searchBar
+        
+        searchController.searchResultsUpdater = self
+        
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchBar.searchBarStyle = .Minimal
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,8 +44,14 @@ class CitiesTableController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cities.count
+        if searchControllerActiveAndNotEmpty(searchController) {
+            return filteredCities.count
+        } else {
+            return cities.count
+        }
     }
+    
+    // MARK: - Table view data source
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCellWithIdentifier(String(UITableViewCell))
@@ -41,57 +64,30 @@ class CitiesTableController: UITableViewController {
         }
         
         let row = indexPath.row
-        let city = cities[row]
+        
+        let city:City
+        if searchControllerActiveAndNotEmpty(searchController) {
+            city = filteredCities[row]
+        } else {
+            city = cities[row]
+        }
         defCell.textLabel?.text = city.name
         defCell.detailTextLabel?.text = String(city.continent)
         
-        
         return defCell
     }
+}
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+extension CitiesTableController: UISearchResultsUpdating {
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        guard let text = searchController.searchBar.text else {
+            return
+        }
+        
+        filteredCities = cities.filter {
+            city in
+            city.name.lowercaseString.hasPrefix(text.lowercaseString)
+        }
+        tableView.reloadData()
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
