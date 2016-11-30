2a5cb6ab1a31f79bb4392c5d0757500cd18a463d
Jonas Reinsch <jonas.reinsch@gmail.com>
2016-11-30 02:18:03 +0100

    add duoslider project

---
+++ DuoSlider/DuoSlider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DuoSlider
+//
+//  Created by Jonas Reinsch on 30/11/2016.
+//  Copyright © 2016 Jonas Reinsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
